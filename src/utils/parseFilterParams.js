8001f71b0445c245ab8c65b63310165609f4a2c8
Oleksiy Kost <AlekseyKo2208@gmail.com>
2025-06-17 11:15:37 +0300

    task-4

---
+++ src/utils/parseFilterParams.js
@@ -0,0 +1,25 @@
+const parseType = (type) => {
+  const isString = typeof type === 'string';
+  if (!isString) return;
+  const isType = (type) => ['work', 'home', 'personal'].includes(type);
+  if (isType(type)) return type;
+};
+
+const parseBoolean = (value) => {
+  const isString = typeof value === 'string';
+  if (!isString) return;
+  if (value === 'true') return true;
+  if (value === 'false') return false;
+};
+
+export const parseFilterParams = (query) => {
+  const { contactType, isFavourite } = query;
+
+  const parsedContactType = parseType(contactType);
+  const parsedIsFavourite = parseBoolean(isFavourite);
+
+  return {
+    contactType: parsedContactType,
+    isFavourite: parsedIsFavourite,
+  };
+};
---
+++ src/validation/contacts.js
@@ -0,0 +1,38 @@
+import Joi from 'joi';
+
+export const createContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required().messages({
+    'string.base': 'Username should be a string',
+    'string.min': 'Username should have at least 3 characters',
+    'string.max': 'Username should have at most 20 characters',
+    'any.required': 'Username is required',
+  }),
+  phoneNumber: Joi.string().min(3).max(20).required(),
+  email: Joi.string().email(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal').required(),
+});
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().min(3).max(20),
+  email: Joi.string().email(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
+});
+
+// const dataToValidate = {
+//   name: 'John Doe',
+//   //   age: 12,
+//   phoneNumber: '+380953444078',
+//   email: 'john.doe@example.com',
+//   isFavourite: true,
+//   contactType: 'home',
+// };
+
+// const validationResult = createContactSchema.validate(dataToValidate);
+// if (validationResult.error) {
+//   console.error(validationResult.error.message);
+// } else {
+//   console.log('Data is valid!');
+// }
